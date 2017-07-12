0dd88d8931d5f7d0d368982ded536f9fd49728ce
John Colanduoni <john@colanduoni.com>
2017-07-11 20:21:01 -0700

    Enable cargo doc/check on non-Darwin platforms

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,7 @@ pub use self::object::{INSObject, NSObject};
 pub use self::string::{INSCopying, INSMutableCopying, INSString, NSString};
 pub use self::value::{INSValue, NSValue};
 
-#[link(name = "Foundation", kind = "framework")]
+#[cfg_attr(any(target_os = "macos", target_os = "ios"), link(name = "Foundation", kind = "framework"))]
 extern { }
 
 #[macro_use]
